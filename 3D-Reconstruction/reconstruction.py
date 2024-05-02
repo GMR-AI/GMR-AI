30c9ec5b4937e92d1f83e560c2d9a9001fb08bcb
adriangt2001 <adriangt2001@hotmail.com>
2024-05-02 19:26:26 +0200

    Get camera info on start

--- 3D-Reconstruction/reconstruction.py
+++ 3D-Reconstruction/reconstruction.py
@@ -4,13 +4,40 @@ import matplotlib.pyplot as plt
 import rclpy
 from rclpy.node import Node
 
+from custom_interfaces.srv import CameraCalibration
 from custom_interfaces.msg import ImageGroup
+
+class CamInfoClient(Node):
+    def __init__(self):
+        super().__init__('cam_info_client_node')
+        self.client = self.create_client(
+            CameraCalibration,
+            'camera_calibration')
+        while not self.client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        self.request = CameraCalibration.Request()
+    
+    def send_request(self):
+        self.future = self.client.call_async(self.request)
+        rclpy.spin_until_future_complete(self, self.future)
+        return self.future.result()
+    
 
 class Reconstruction(Node):
     def __init__(self):
         super().__init__('reconstruction_node')
         self.subscription = self.create_subscription(ImageGroup, 'cam_images', self.listener_callback, 10)
-    
+        self.cam_info = self.get_cam_info()
+        print(self.cam_info)
+
+
+    def get_cam_info(self):
+        cam_info_client = CamInfoClient()
+        cam_info = cam_info_client.send_request()
+        cam_info_client.destroy_node()
+        return cam_info
+
+
     def listener_callback(self, msg: ImageGroup):
         # Process the image here
         # msg holds the array in msg.data
