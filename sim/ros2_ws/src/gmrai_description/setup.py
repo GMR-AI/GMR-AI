2c26c8d13e54118f8fa5aeba4f91dcff5a64757a
adriangt2001 <adriangt2001@hotmail.com>
2024-05-13 18:00:37 +0200

    Launch file to broadcast transformations

---
+++ sim/ros2_ws/src/gmrai_description/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+import os
+from glob import glob
+
+package_name = 'gmrai_description'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='adriangt2001',
+    maintainer_email='adriangt2001@hotmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
