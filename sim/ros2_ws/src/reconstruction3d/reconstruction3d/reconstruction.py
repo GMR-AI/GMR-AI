ae32ba37a5b57677ace0fa7f008cce593244809f
adriangt2001 <adriangt2001@hotmail.com>
2024-05-23 17:27:23 +0200

    Save first 4 images sent by simulation camera

--- sim/ros2_ws/src/reconstruction3d/reconstruction3d/reconstruction.py
+++ sim/ros2_ws/src/reconstruction3d/reconstruction3d/reconstruction.py
@@ -8,6 +8,8 @@ from rclpy.node import Node
 from sensor_msgs.msg import CameraInfo
 from sensor_msgs.msg import CompressedImage
 from custom_interfaces.msg import ImageCamInfoGroup
+from cv_bridge import CvBridge
+bridge = CvBridge()
     
 
 class Reconstruction(Node):
@@ -18,9 +20,17 @@ class Reconstruction(Node):
         self.image_group_subscriber = self.create_subscription(ImageCamInfoGroup, 'cams/image_group', self.callback, 10)
         self.image_group_subscriber
 
+        self.counter = 0
+
     def callback(self, image_cam_info_group):
-        # Do reconstruction here
-        pass
+        if self.counter > 3: return
+
+        for im_cam_info in image_cam_info_group.image_cam_info_list:
+            imgmsg = im_cam_info.img
+            im = bridge.imgmsg_to_cv2(imgmsg, 'bgr8')
+            im = cv2.flip(im, 0)
+            cv2.imwrite('/home/adriangt2001/Pictures/robot_cam'+'{:02d}'.format(self.counter)+'.jpg', im)
+            self.counter += 1
 
 
 def main():
